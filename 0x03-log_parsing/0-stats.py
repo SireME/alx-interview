f833b4ca6506b87bec681fdd494a929b2b0cf865
Mbang Ernest N <mbangernest@yahoo.com>
2024-01-25 15:50:36 +0100

    create log parser

---
+++ 0x03-log_parsing/0-stats.py
@@ -0,0 +1,55 @@
+#!/usr/bin/python3
+"""
+This is a script that reads stdin
+line by line and computes metrics
+"""
+import sys
+import re
+
+line_num = 0
+statuscode_dic = {}
+total_size = 0
+
+try:
+    for line in sys.stdin:
+        rq = "GET /projects/260 HTTP/1.1"
+        pa = rf'^(\d+\.\d+\.\d+\.\d+) - \[([^\]]+)\] "{rq}" (\d+) (\d+)$'
+        match = re.match(pa, line)
+        if match:
+            # track number of match lines
+            line_num += 1
+
+            # necessary data
+            ip_address = match.group(1)
+            date = match.group(2)
+            try:
+                status_code = int(match.group(3))
+            except ValueError:
+                status_code = ""
+            file_size = int(match.group(4))
+
+            # compute number of occurrences of status codes
+            if status_code in statuscode_dic:
+                statuscode_dic[status_code] += 1
+            else:
+                statuscode_dic[status_code] = 1
+
+            # compute total file size per line
+            total_size += file_size
+
+        if line_num == 10:
+            print(f"File size: {total_size}")
+            sorted_dic = dict(sorted(statuscode_dic.items()))
+            for key, value in sorted_dic.items():
+                print(f"{key}: {value}")
+
+            # reset values back for the next log group
+            line_num = 0
+            statuscode_dic = {}
+            total_size = 0
+
+except KeyboardInterrupt:
+    print(f"File size: {total_size}")
+    sorted_dic = dict(sorted(statuscode_dic.items()))
+    for key, value in sorted_dic.items():
+        print(f"{key}: {value}")
